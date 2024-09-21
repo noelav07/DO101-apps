4ea781ef4a967fa2790f3e6c96368f7c2296d422
noelav07 <noel.a.v.07@gmail.com>
2024-09-21 05:34:17 +0000

    added new variable

--- version/app.js
+++ version/app.js
@@ -7,8 +7,11 @@ app.get('/', function (req, res) {
 
     response = 'This is version 2 of the app.' + '\n';
 
+    var2 = 'Variable 2' + '\n';
+
     //send the response to the client
     res.send(response);
+    res.send(var2);
 
 });
 
