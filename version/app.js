e7c78eb00285ccc41df8c5cfe74ed8298309d685
noelav07 <noel.a.v.07@gmail.com>
2024-09-21 06:04:09 +0000

    version 3

--- version/app.js
+++ version/app.js
@@ -2,17 +2,10 @@ const express = require('express');
 app = express();
 
 var response;
-
  
-
 app.get('/', function (req, res) {
 
     response = 'This is version 3 of the app.' + '\n';
-
-    var2 = 'Variable 2' + '\n';
-
-    //send the response to the client
-//    res.send(response);
 
 });
 
